e2bf982e9f7b66f00cd016e65bb454a2db77bd69
Daria <darya.piankova@yandex.ru>
2023-02-05 15:38:11 +0500

    Tsk 13

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿Console.WriteLine("Input three-digit number\n");
-int a = Convert.ToInt32(Console.ReadLine());
-a=a%100;
-
-Console.WriteLine($"{a/10}");
+﻿
+Console.WriteLine("Input an integer\n");
+string a = Console.ReadLine();
+if(a.Length<3){
+    Console.WriteLine("There is no third number in the integer\n");
+}else{
+Console.WriteLine($"{a[2]}\n");
+}
